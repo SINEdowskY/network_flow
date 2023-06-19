f636d6c7fc2285f788c6f545dbaa112cc7e0223a
SINEdowskY <kub200105@gmail.com>
2023-06-19 20:37:00 +0200

    update busacker gowen

--- busacker_gowen.py
+++ busacker_gowen.py
@@ -1,105 +1,69 @@
 import networkx as nx
-import numpy as np
-from typing import List, Tuple
-import random
 
-def bellman_ford(G: nx.DiGraph, source: int) -> np.ndarray:
-    distance = np.full(len(G.nodes), np.inf)
-    distance[source] = 0
+def bellman_ford_shortest_augmenting_path(graph, source, sink):
+    # 1. Inicjalizuj odległości wszystkich wierzchołków jako nieskończoność,
+    # z wyjątkiem wierzchołka źródłowego, którego odległość ustaw na 0.
+    distances = {node: float('inf') for node in graph.nodes}
+    distances[source] = 0
+    predecessors = {}
 
-    for _ in range(len(G.nodes) - 1):
-        for u, v, attr in G.edges(data=True):
+    # 2. Wykonaj relaksację krawędzi dla każdej krawędzi grafu.
+    # Relaksacja oznacza sprawdzenie, czy możliwe jest skrócenie aktualnej odległości do danego wierzchołka
+    # poprzez skorzystanie z danej krawędzi.
+    # Jeśli tak, zaktualizuj odległość wierzchołka na wartość mniejszą.
+    # Powtarzaj krok 2 dla wszystkich krawędzi grafu V-1 razy, 
+    # gdzie V to liczba wierzchołków.
+    # To pozwala na rozważenie najdłuższych ścieżek w grafie.
+
+    for _ in range(len(graph.nodes) - 1):
+        for u, v, attr in graph.edges(data=True):
             weight = attr['weight']
-            if distance[u] + weight < distance[v]:
-                distance[v] = distance[u] + weight
-
-    return distance
-
-def dijkstra(G: nx.DiGraph, source: int) -> np.ndarray:
-    distance = np.full(len(G.nodes), np.inf)
-    distance[source] = 0
-
-    visited = np.zeros(len(G.nodes), dtype=bool)
-
-    while True:
-        u = -1
-        min_distance = np.inf
-        for v in range(len(G.nodes)):
-            if not visited[v] and distance[v] < min_distance:
-                min_distance = distance[v]
-                u = v
-
-        if u == -1:
-            break
-
-        visited[u] = True
-
-        for v, attr in G.adj[u].items():
             capacity = attr['capacity']
             flow = attr['flow']
-            weight = attr['weight']
-            if distance[u] + weight < distance[v] and flow < capacity:
-                distance[v] = distance[u] + weight
+            if flow < capacity and distances[u] + weight < distances[v]:
+                distances[v] = distances[u] + weight
+                predecessors[v] = u
 
-    return distance
+    # Sprawdzenie czy istnieje ścieżka powiększająca
+    if sink not in predecessors:
+        return None
 
-def mincost_maxflow(G: nx.DiGraph, source: int, sink: int) -> Tuple[int, int]:
-    total_cost = 0
-    total_flow = 0
+    # Odtworzenie ścieżki powiększającej
+    path = [sink]
+    while path[-1] != source:
+        path.append(predecessors[path[-1]])
+    path.reverse()
 
-    while True:
-        distance = dijkstra(G, source)
-        if distance[sink] == np.inf:
-            break
+    return path
 
-        path = [sink]
-        flow = np.inf
+def min_cost_max_flow(graph, source, sink):
+    flow_value = 0
+    cost_value = 0
+    path = bellman_ford_shortest_augmenting_path(graph, source, sink)
 
-        while True:
-            u = path[-1]
-            if u == source:
-                break
-
-            for v, attr in G.succ[u].items():
-                capacity = attr['capacity']
-                f = min(capacity - attr['flow'], flow)
-                if f > 0:
-                    flow = f
-                    path.append(v)
-                    break
-
-            if u == path[-1]:
-                path.pop()
-
+    while path is not None:
+        # Wyszukiwanie maksymalnego przepływu na ścieżce powiększającej
+        min_capacity = float('inf')
         for i in range(len(path) - 1):
-            u = path[i]
-            v = path[i + 1]
-            G.edges[u, v]['flow'] += flow
-            G.edges[v, u]['flow'] -= flow
+            u, v = path[i], path[i + 1]
+            capacity = graph[u][v]['capacity']
+            flow = graph[u][v]['flow']
+            min_capacity = min(min_capacity, capacity - flow)
 
-        cost = 0
-        for u, v, attr in G.edges(data=True):
-            cost += attr['flow'] * attr['weight']
+        # Aktualizacja przepływu i kosztu
+        for i in range(len(path) - 1):
+            u, v = path[i], path[i + 1]
+            if graph.has_edge(u, v):
+                graph[u][v]['flow'] += min_capacity
+                graph[u][v]['cost'] = -graph[u][v]['weight']
+            if graph.has_edge(v, u):
+                graph[v][u]['flow'] -= min_capacity
+                graph[v][u]['cost'] = graph[v][u]['weight']
+            cost_value += min_capacity * graph[u][v]['weight']
 
-        total_cost += cost
-        total_flow += flow
+        flow_value += min_capacity
 
-    return total_cost, total_flow
+        # Wyszukiwanie kolejnej ścieżki powiększającej
+        path = bellman_ford_shortest_augmenting_path(graph, source, sink)
 
-# Przykładowe użycie
-graph = nx.DiGraph()
-graph.add_nodes_from(range(8))
-u_edge = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 5]
-v_edge = [1, 2, 3, 2, 3, 4, 3, 5, 6, 4, 5, 5, 7, 7, 5, 7, 6]
-weights = [random.randint(1, 7) for _ in range(17)]
-capacities = [random.randint(1, 7) for _ in range(17)]
-
-for u, v, weight, capacity in zip(u_edge, v_edge, weights, capacities):
-    graph.add_edge(u, v, weight=weight, capacity=capacity, flow=0)
-
-source = 0
-sink = 6
-
-min_cost, max_flow = mincost_maxflow(graph, source, sink)
-print("Minimalny koszt przepływu:", min_cost)
-print("Maksymalny przepływ:", max_flow)
+    return flow_value, cost_value
